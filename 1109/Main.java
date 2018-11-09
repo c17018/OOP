2253deaf6c1bf1b57211173ffab104f50b0e2553
c17018 <c17018@std.it-college.ac.jp>
2018-11-09 20:12:15 +0900

    edit name

--- 1109/Main.java
+++ 1109/Main.java
@@ -8,10 +8,10 @@ public class Main
     public static void main(String[] args)
     {
         Factory factory  = new AccountFactory();
-        Product account1 = factory.create("Ralph Johnson", "aaa");
-        Product account2 = factory.create("Richard Helm", "bbb");
-        Product account3 = factory.create("John Vlissides", "ccc");
-        Product account4 = factory.create("Erich Gamma", "ddd");
+        Product account1 = factory.create("ムササビ", "007");
+        Product account2 = factory.create("CCO", "SecretSwordTsu");
+        Product account3 = factory.create("HAa", "0000");
+        Product account4 = factory.create("daigo", "daigo3DAIgosun");
 
         account1.use();
         account2.use();
